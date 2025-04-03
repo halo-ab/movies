6c8a80b1a049fbb33b5433bb9d8e5917876e9bee
halo-ab <d33717154@gmail.com>
2025-04-03 21:30:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7526346918:AAEtkpqz8AkurC_Q_GKz9S-OBB0fyUC
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6553258392').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/dmin_Deep") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/Anonymous_d33p") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002649444486'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+oQSpiAucbQ8zYzc1')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002610319459').split()]
